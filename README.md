dfc1d3a4d2005027e453a848f7fca01c17e83a4d
JHEastman <ceramic_mug@outlook.com>
2019-08-07 11:36:01 -0400

    Functional General geoProcessing program

--- README.md
+++ README.md
@@ -2,20 +2,26 @@
 
 This repository holds an annotated copy of the scripts and workflows that I put together for the PEI 2019 Farm Project. This README file will give you a detailed overview of the work. All files and folders referenced here are within this repository. To access and use the code, simply download this repository as a zip file and follow the steps below for installing python and necessary python modules.
 
+I wish you well!
+
+## Table of Contents
+
 - [FARM PROJECT 2019 ~ Joshua Eastman](#farm-project-2019--joshua-eastman)
+  - [Table of Contents](#table-of-contents)
   - [Project Arms](#project-arms)
   - [DRONE](#drone)
     - [FieldAgent](#fieldagent)
       - [Image Import](#image-import)
       - [NDVI Mosaic Output](#ndvi-mosaic-output)
     - [Construct Shapefiles to Define Areas of Interest (AOI)](#construct-shapefiles-to-define-areas-of-interest-aoi)
+    - [Using Python for Bulk GeoTIFF Processing](#using-python-for-bulk-geotiff-processing)
   - [ARABLE](#arable)
     - [Getting the Data](#getting-the-data)
+      - [Limitations](#limitations)
     - [Handling the Data](#handling-the-data)
 
-I wish you well!
-
 ## Project Arms
+
 1. [Drone Data](##DRONE)
 2. [Arable Sensor Data](##ARABLE)
 3. Other Data
@@ -35,9 +41,11 @@ I wish you well!
 Each of these project arms were approached somewhat differently due to differences in the data structures. The Arable Sensor and Drone Imagery data wrangling applications presented in this repository are built around the Arable and FieldAgent specifications, respectively. Bug, soil nutrient, and camera trap data are all held in spreadsheets and so are handled much more simply. We will begin our overview with a detailed description of Arable data handling.
 
 ## DRONE
+
 **Drone image processing and data extraction** is the big ticket item. This section describes how to use a GIS application to define areas of interest (AOI) and then use these areas with Sentera FieldAgent NDVI exports to extract matrices of NDVI values. Once you have an extracted NDVI value matrix, you can perform all the numerical analysis you want, such as distribution fitting and differentials over time. Ok, let's get started:
 
 *What you need:*
+
 1. [QGIS](https://qgis.org/en/site/)
 2. [Sentera FieldAgent Desktop](https://sentera.com/fieldagent-platform/)
 3. The following Python modules:
@@ -50,6 +58,7 @@ Each of these project arms were approached somewhat differently due to differenc
 #### Image Import
 
 Here is how structured my image import workflow, and it worked really well. The basic steps are:
+
 1. Move RGB and NIR images to folder on computer
     - RGB: Create folders for each farm and date
     - NIR: Add farm name to snapshot folders after moving
@@ -57,6 +66,7 @@ Here is how structured my image import workflow, and it worked really well. The 
     - Can add multiple farms at once, **but not multiple times**. So if any farm was flown over more than once between imports, you need to import each flight seperately.
 
 After having successfully flown the drone at your farms for the week, you need to import the images from the two drone SD cards:
+
 - From the side of the drone (RGB)
 
 <p align="center">
@@ -64,6 +74,7 @@ After having successfully flown the drone at your farms for the week, you need t
 </p>
 
 - From the little green Sentera sensor (NIR)
+  
 <p align="center">
 <img src='./assets/NIRsensor.jpg' width=30%/>
 </p>
@@ -96,9 +107,9 @@ The contents of each RGB child folder (`PU-2019-08-02-RGB`, etc.) are the images
 
 After moving everything onto the computer and cleaning the SD cards, it's time to import the images as "Surveys" in the FieldAgent Desktop app.
 
-**Launch FieldAgent Desktop Application**
 <p align="center">
-<img src='./assets/ClickFieldAgent.png' width=80%/>
+<img src='./assets/ClickFieldAgent.png' width=80%/></br>
+Launch FieldAgent Desktop Application
 </p>
 
 Once you're inside the applicaion, click the big blue "CREATE SURVEYS" button at the top. Click "Add" on the right of the white box, and **select the NIR and RGB folders** that you've migrated over to the computer. You can choose folders that correspond to multiple farms and the FieldAgent software will sort them out. What it can't sort out is time, so make sure that you only include one flight tops per farm. Go through the next steps (see [Sentera's Documentation](https://desk.zoho.com/portal/sentera/kb/articles/fieldagent-desktop-user-guide)for this) and everything will import properly to their respective farms.
@@ -126,6 +137,7 @@ If everything goes properly, it should take anywhere between 30 minutes and 4 ho
 </p>
 
 **To prepare the NDVI mosic for export**:
+
 1. Select "Full Mosaic NDVI" in the left sidebar and allow it to load onto the map (see photo above)
 2. Click on the "NDVI Toolbox" rectangle at the top-left corner of the map
 3. Under "Statistics", increase bin size to max (20)
@@ -173,6 +185,7 @@ For demonstration, I'll walk you through sectioning the PU corn field. Farm Proj
 I accomplished this using [QGIS](https://qgis.org/en/site/). Go ahead and install it to follow along.
 
 **Setting Up the Workspace:**
+
 1. Launch QGIS
 2. If you don't have a project specified for your Farm Project (or other) work, create a new project
     - Click the button at the top-left of the window, "New Project"
@@ -185,6 +198,7 @@ I accomplished this using [QGIS](https://qgis.org/en/site/). Go ahead and instal
     - If your map viewer gets all jacked up and everything dissapapears (this happened to me all the time as I was learning QGIS), right-click on a layer and select "Zoom to Layer." This should restore your view to something manageable.
 
 **Creating the AOI file:**
+
 1. With an RGB mosaic of choice open on the display, click "New Shapefile Layer" in the top-left of the window
     - The icon looks like a little V with dots on it
 2. In "Filename", click the box all the way to the right with three little dots to open a file browser. Navigate to where you want to store your shapefile and name it something conventional
@@ -218,9 +232,14 @@ Creating Polygon Layer in QGIS
 7. When you've finished creating polygons for all your areas of interest, click "Toggle Editing" again
     - Save when prompted
 
-And that's it! If you took your time and defined your polygons well, then your analysis will be ON POINT! If not, then your numbers will lie, so make sure you do all this well.
+And that's it! If you took your time and defined your polygons well, then your analysis will be ON POINT! If not, then your numbers will lie, so make sure you define your AOIs very well.
+
+### Using Python for Bulk GeoTIFF Processing
+
+I wrote the [geoProcessing.py](./src/geoProcessing.py) file to take all of the exported GeoTIFF files TODO:
 
 ## ARABLE
+
 The arable sensors are stationary UFO-shaped things on poles that stick up above crop canopies. They gather atmospheric and spectrometric data and report the data in two resolutions: daily and hourly.
 
 - Link to the Arable website: [https://www.arable.com/](https://www.arable.com/)
@@ -230,16 +249,20 @@ The arable sensors are stationary UFO-shaped things on poles that stick up above
 I know that last link looks sketchy, and you'll be directed to a really sketchy-looking page that says "FREE PLAN," but that's actually the documentation page. Go ahead and click through the scary blue button and you'll land on the true Arable API documentation site. I used it to build my program, so I know it's the right stuff.
 
 ### Getting the Data
+
 - File: [./src/ArableGrep.py](./src/ArableGrep.py)
 - Function: Downloads all the hourly, daily, and health data from currently operational Arable sensors and outputs all data to csv files in the [arable_data](./arable_data) directory, labeled by sensor name and data type.
 
 To run this program, navigate to the folder containing this repository using your terminal and run the following command:
+
 ```bash
 python ./src/ArableGrep.py
 ```
+
 This will create an "arable_data" directory within the parent directory (same level as "src") and create csv files containing hourly, daily, and health data for all of the sensors within that folder. If any of these csv files already exists, the program will intelligently download all of the most recent data (between the last sync and the present) and append it to the existing csv file.
 
-After running this program, your "arable_data" directory should look something like this: 
+After running this program, your "arable_data" directory should look something like this:
+
 ```bash
 .
 ├── arable_data
@@ -258,13 +281,16 @@ After running this program, your "arable_data" directory should look something l
     └── ArableGrep.py
 ```
 
-**Limitations**
+#### Limitations
+
 Three known bugs exist in this program:
+
 - **THIS METHOD FOR GETTING THE DATA WORKS AS LONG AS THE SENSOR IS ON AND ACTIVELY REPORTING.** If the sensor is turned off or nonfunctional, THIS METHOD WILL NOT WORK because the sensor will not be in the list of active sensors accessed by this program. But as long as all sensors are on and active, this program will work great.
 - When the program appends new data to old csv files, it sometimes adds a blank row between the old and new data.
 - The program sometimes creates odd "_hourly", "_daily", and "_health" csv files with no sensor name labels. I believe these correspond to sensors that were operational but which for whatever reason are off or down. Disregard these files. If you can figure out a way to get sensor names for those sensors that are not currently operational, that would be ideal, because then the program could keep writing to those specific files.
 
 ### Handling the Data
+
 Downloading the data into an accessible (csv) format is the biggest hurdle. Once it's in the csv format, you can do whatever you want with it. When I did my pretty basic analysis of the Princeton cornfield sensor data, I only used the "*_daily.csv" data and averaged data per drone flight week. I'll discuss that in more detail later.
 
 However, early on in the summer I built a wrangling program with some handy functions that take arable csv files as inputs and output handy data objects. You're welcome to use these functions and dictionaries for your own programs.
@@ -272,4 +298,3 @@ However, early on in the summer I built a wrangling program with some handy func
 - General Wrangling Program: [./src/ArableWrangle.py](./src/ArableWrangle.py)
 
 Not all of the functions in [ArableWrangle](./src/ArableWrangle.py) may be useful to you. I would look through this script and identify things that are helpful, implementing only those aspects that are benificial. This program wasn't cleanly finished and packaged because my work shifted to Princeton cornfield only at the end of the 2019 Farm Project, which meant that handling *all* the Arable data was no longer important to me. Development of this program therefore ground to a halt.
-
---
+++ src/geoProcessing.py
@@ -0,0 +1,402 @@
+##########################################
+##                                      ##
+##     WRITTEN BY JOSHUA EASTMAN        ##
+##            summer 2019               ##
+##                                      ##
+##########################################
+
+##########################################
+##                                      ##
+##          RUNNING THE PROGRAM         ##
+##                                      ##
+##    python geoProcessing.py [farm]    ##
+##                                      ##
+##########################################
+##                                      ##
+##                                      ##
+##      NECESSARY FOLDER STRUCTURE      ##
+##       and FILE NAME CONVENTIONS      ##
+##                                      ##
+##     parent                           ##
+##       |- geoProcessing.py            ##
+##       |- farm1_NDVI_YYYY_MM_DD.tif   ##
+##       |- farm1_RGB_YYYY_MM_DD.tif    ##        
+##       |- farm2_NDVI_YYYY_MM_DD.tif   ##
+##       |- farm2_RGB_YYYY_MM_DD.tif    ##    
+##       |- AOI                         ##
+##           |- farm1.shp               ##
+##           |- farm2.shp               ##
+##       etc                            ##
+##                                      ##
+##########################################
+
+####### MODULES #########
+
+# For handling command-line arguments
+import sys
+
+# For going through files and getting all those with a certain specification
+import glob
+
+# For handling the GeoTIFF files
+import rasterio
+
+# For visualizing our GeoTIFFs
+from rasterio.plot import show
+
+# For building spreadsheet-like dataframes
+import pandas as pd
+
+# For performing computations and such
+import numpy as np
+
+# For handlish shapefile AOI layers
+import geopandas as gpd
+
+# For text processing to get what we want out of Strings
+import re
+
+# For date and time handling and computing
+import datetime
+
+# For data visualization
+import matplotlib.pyplot as plt
+
+# For tuning input GeoTIFFs to AOI layers
+from shapely.geometry import mapping
+from rasterio.plot import plotting_extent
+from rasterio.mask import mask
+
+# For traversing and interacting with folder structure
+import os
+
+# For more data visualization
+import seaborn as sns
+sns.set(rc={'figure.figsize':(6,6)})
+
+#######################################################################
+############## Lasciate ogne speranza, voi ch'intrate #################
+#######################################################################
+
+
+
+# Take in a filename with the correct naming convention and
+# return a datetime object of the corresponding date
+def dayParse(filename):
+    parser = re.compile('.*_(.*)_(.*)_(.*).tif')
+
+    try:
+        date = parser.match(filename)
+    except:
+        raise Exception('Filename not properly formatted. Please conclude NDVI and RGB filenames with _YYYY_MM_DD.tif')
+
+    return datetime.datetime(int(date.group(1)),int(date.group(2)),int(date.group(3)))
+
+def getAOIs():
+    try:
+        aois = gpd.read_file(os.path.join(my_path,'./AOI/'+farm+'.shp'))
+    except:
+        raise Exception('No shapefile for '+farm+' in AOI folder')
+
+    return aois
+
+def getFields():
+    # Check to see if defining shapefile layer exists
+    fields_shapes = glob.glob('./FIELD/'+farm+'*.shp')
+
+    # If none exist, exit
+    if len(fields_shapes) < 1:
+        print('No field-defining shapefiles found. Continuing other processes')
+        return
+
+    return gpd.read_file(fields_shapes[0])
+
+# Go through all the FieldAgent NDVI GeoTIFFs for this farm and output new TIFFs with pure NDVI pixel data [-1, 1]
+def makeNDVI():
+
+    # outside NDVI range
+    nodataval = 2
+
+    # List to hold all flight dates for this farm
+    dates = []
+
+    # Run through the all the ndvi mosaics for this farm
+    for tif in ndvi_r:
+        with rasterio.open(tif) as src:
+
+            # record the date
+            date = dayParse(tif)
+            dates.append(date)
+
+            # compute NDVI
+            print('Computing NDVI for '+farm+': '+date.strftime('%Y-%m-%d'))
+            intensity = src.read(1,masked=True).astype(np.float)
+            NDVI = -(((intensity-255/2)/255)*2) # because each of the three channels is equal since this is a grayscale
+
+            # fix metadata
+            NDVI = np.ma.filled(NDVI,fill_value=nodataval)
+            NDVI_meta = src.meta.copy()
+            NDVI_meta.update({'nodata':nodataval})
+            NDVI_meta.update({'count':1})
+            NDVI_meta.update({'dtype':'float64'})
+
+            # output file
+            filename = farm+'_trueNDVI_{}.tif'.format(date.strftime('%Y_%m_%d'))
+            out_path = output_dict['wholefield_ndvi']+filename
+
+            print('Writing whole field: '+out_path)
+            with rasterio.open(out_path, 'w', **NDVI_meta) as out:
+                out.write(NDVI, 1)
+    
+    print('Finished converting Sentera exports to true NDVI raster GeoTIFFs')
+
+    # Return the list of dates to know how many files we are dealing with and 
+    # to have handles for each of them based on their output names
+    return dates
+
+# Input a list of dates and create aoi-specific ndvi csv files
+# and ndvi cropped GeoTIFFs for each
+def aoiNDVI(dates):
+
+    nodataval = 2
+
+    bigPapaNDVI = farm + '_bigPapa.csv'
+    bigPout = output_dict['ndvi_csv']+bigPapaNDVI
+    with open(bigPout,'w') as DUDE:
+        DUDE.write('aoi,date,val')
+
+    aois = getAOIs()
+
+    # Go through all of the time steps
+    for date in dates:
+
+        # Input filename, produced using makeNDVI()
+        NDVI_in = output_dict['wholefield_ndvi']+farm+'_trueNDVI_{}.tif'.format(date.strftime('%Y_%m_%d'))
+
+        # get NDVI for each aoi seprately
+        with rasterio.open(NDVI_in) as NDVI_true:
+
+            # crop by aoi
+            for index, row in aois.iterrows():
+
+                # Make the polygon definition into something we can use
+                poly = mapping(row['geometry'])
+                ndvi_mask, ndvi_affine = mask(NDVI_true,[poly],crop=True)
+
+                output = output_dict['ndvi_csv']+farm+'_aoi_ndvi_'+row['Kind']+'_'+date.strftime('%Y-%m-%d')+'.csv'
+
+                # Output to individual files
+                print('Writing NDVI pixel data to csv:\n    '+output)
+
+                vals = ndvi_mask.flatten('F')
+                vals = vals[vals != 2.]
+                np.savetxt(output,vals,delimiter=",")
+
+                print('Appending to ' + farm + ' Bigpapa: '+bigPout)
+                
+                # Append to by-farm Bigpapa
+                with open(bigPout,'a') as bigBoy:
+                    for val in vals:
+                        newline ='\n'+row['Kind']+','+date.strftime('%Y-%m-%d')+','+str(val)
+                        bigBoy.write(newline)
+
+                # Append mean to mean_csv
+                print('Appending mean to mean NDVI master csv')
+                meanNDVI(vals,row,date)
+
+                del(vals)
+
+                # fix metadata
+                ndvi_mask = np.ma.filled(ndvi_mask,fill_value=nodataval)
+                ndvi_mask_meta = NDVI_true.meta.copy()
+                ndvi_mask_meta.update({'nodata':nodataval,
+                                    'transform': ndvi_affine,
+                                    'height':ndvi_mask.shape[1], # ndarray rows
+                                    'width':ndvi_mask.shape[2]}) # ndarray cols
+
+                mask_out = farm+'_trueNDVI_{}_{}.tif'.format(row['Kind'],date.strftime('%Y-%m-%d'))
+                out_path = output_dict['treatment_ndvi']+mask_out
+
+                print('Building tiff for '+farm+' '+row['Kind']+': '+out_path)
+
+                with rasterio.open(out_path, 'w', **ndvi_mask_meta) as out:
+                    out.write(ndvi_mask)
+
+                del(ndvi_mask,ndvi_affine,ndvi_mask_meta)
+
+# Take the true NDVI output that's the size of the Sentera FieldAgent output
+# and crop it to defined "field" sections
+def field_ndviCROP(dates):
+
+    # Get the field shapefiles
+    fields_shapes = getFields()
+
+    nodataval = 2
+
+    for date in dates:
+        # Input filename, produced using makeNDVI()
+        NDVI_in = output_dict['wholefield_ndvi']+farm+'_trueNDVI_{}.tif'.format(date.strftime('%Y_%m_%d'))
+        with rasterio.open(NDVI_in) as NDVI_true:
+            for i in range(len(fields_shapes)):
+
+                field_mask, field_affine = mask(NDVI_true,[fields_shapes['geometry'][i]],crop=True)
+                field_mask = np.ma.filled(field_mask,fill_value=nodataval)
+                field_mask_meta = NDVI_true.meta.copy()
+                field_mask_meta.update({'nodata':nodataval,
+                                        'transform': field_affine,
+                                        'height':field_mask.shape[1], # ndarray rows
+                                        'width':field_mask.shape[2]}) # ndarray cols
+
+                mask_out = farm+str(i)+'_trueNDVI_field_{}.tif'.format(date.strftime('%Y-%m-%d'))
+                field_out = output_dict['field_ndvi']+mask_out
+
+                print('Writing field true NDVI tif: ' + field_out)
+
+                with rasterio.open(field_out, 'w', **field_mask_meta) as out:
+                    out.write(field_mask)
+
+
+# Take Senetera output RGB mosaic and crop it to defined "field" sections
+def field_rgbCrop(dates):
+
+    # Get the field shapefiles
+    fields_shapes = getFields()
+
+    for date in dates:
+        with rasterio.open(os.path.join(my_path,farm+'/'+farm+'_RGB_'+date.strftime('%Y_%m_%d')+'.tif')) as rgb:
+            for i in range(len(fields_shapes)):
+
+                rgb_mask, rgb_affine = mask(rgb,[fields_shapes['geometry'][i]],crop=True)
+
+                rgb_mask = np.ma.filled(rgb_mask)
+                rgb_mask_meta = rgb.meta.copy()
+                rgb_mask_meta.update({'transform': rgb_affine,
+                                        'height':rgb_mask.shape[1], # ndarray rows
+                                        'width':rgb_mask.shape[2]}) # ndarray cols
+
+                rgb_out = farm+str(i)+'_rgb_field_{}.tif'.format(date.strftime('%Y-%m-%d'))
+                field_out = output_dict['field_rgb']+rgb_out
+
+                print('Writing field rgb tif: ' + field_out)
+
+                with rasterio.open(field_out, 'w', **rgb_mask_meta) as out:
+                    out.write(rgb_mask)
+
+
+# crop and output aoi NDVI GeoTIFFs for graphics production
+def aoi_ndviCROP(dates):
+
+    aois = getAOIs()
+
+    for date in dates:
+        print('Cropping ndvi by kind : '+date.strftime('%Y-%m-%d'))
+        with rasterio.open(os.path.join(my_path,farm+'/'+farm+'_NDVI_'+date.strftime('%Y_%m_%d')+'.tif')) as src_n:
+            for index, row in aois.iterrows():
+                nd_mask, nd_affine = mask(src_n,[row['geometry']],crop=True)
+                nd_mask_meta = src_n.meta.copy()
+
+                nd_mask_meta.update({'transform': nd_affine,
+                                    'height':nd_mask.shape[1], # ndarray rows
+                                    'width':nd_mask.shape[2]}) # ndarray cols
+
+                nd_out = farm+'_NDVI_'+row['Kind']+'_cropped_{}.tif'.format(date.strftime('%Y-%m-%d'))
+
+                nd_out_path = output_dict['treatment_ndvi']+nd_out
+
+                with rasterio.open(nd_out_path, 'w', **nd_mask_meta) as out:
+                    out.write(nd_mask)
+                    print('Outputting ndvi: ' + nd_out_path)
+
+# crop and output aoi RGB GeoTIFFs for graphics production
+def aoi_rgbCROP(dates):
+
+    aois = getAOIs()
+
+    for i in range(len(dates)):
+        print('Cropping rgb by kind : '+dates[i].strftime('%Y-%m-%d'))
+        with rasterio.open(rgb_r[i]) as src_r:
+            for index, row in aois.iterrows():
+                rgb_mask, rgb_affine = mask(src_r,[row['geometry']],crop=True)
+                rgb_mask_meta = src_r.meta.copy()
+                rgb_mask_meta.update({'transform': rgb_affine,
+                                    'height':rgb_mask.shape[1], # ndarray rows
+                                    'width':rgb_mask.shape[2]}) # ndarray cols
+
+                rgb_out = farm+'_RGB_'+row['Kind']+'_cropped_{}.tif'.format(dates[i].strftime('%Y-%m-%d'))
+
+                rgb_out_path = output_dict['treatment_rgb']+rgb_out
+
+                with rasterio.open(rgb_out_path, 'w', **rgb_mask_meta) as out:
+                    out.write(rgb_mask)
+                    print('Outputting rgb: ' + rgb_out_path)
+
+# Creates one master csv that contains the average NDVI 
+# value for each aoi at each time step
+def meanNDVI(vals,row,date):
+    output = output_dict['mean_csv']+farm+'_mean_ndvi.csv'
+    if not os.path.exists(output):
+        with open(output,'w') as DUDE:
+            DUDE.write('aoi,date,mean_ndvi')
+
+    # Append to by-farm Bigpapa
+    with open(output,'a') as YES:
+        newline ='\n'+row['Kind']+','+date.strftime('%Y-%m-%d')+','+str(np.mean(vals))
+        YES.write(newline)
+    
+    return
+
+# Function to call all the above functions in proper order
+def allTheThings():
+    dates = makeNDVI()
+    field_ndviCROP(dates)
+    field_rgbCrop(dates)
+    aoi_ndviCROP(dates)
+    aoi_rgbCROP(dates)
+    aoiNDVI(dates)
+
+#############################################
+############### MAIN METHOD #################
+#############################################
+
+# Get the farm name
+farm = sys.argv[1]
+
+# Get my current path
+my_path = os.path.abspath(os.path.dirname(__file__))
+
+# handle ndvi mosaic GeoTIFFs
+ndvi_r = glob.glob(os.path.join(my_path,'./'+farm+'/'+farm+'_NDVI*.tif'))
+
+# kill the program if we don't have what we need
+if len(ndvi_r) < 1:
+    raise Exception('No NDVI GeoTIFFs corresponding to ' + farm + ' can be found. Is your file structure correct?')
+
+# handle rgb mosaic GeoTIFFs
+rgb_r = glob.glob(os.path.join(my_path,'./'+farm+'/'+farm+'_RGB*.tif'))
+
+if len(rgb_r) < 1:
+    raise Exception('No RGB GeoTIFFs corresponding to ' + farm + ' can be found. Is your file structure correct?')
+
+# List directories for storing outputs
+output_dict = {
+    'field_ndvi': os.path.join(my_path, './OUTPUTS/NDVI_Field_tiffs/'+farm+'/'),
+    'field_rgb': os.path.join(my_path, './OUTPUTS/RGB_Field_tiffs/'+farm+'/'),
+    'wholefield_ndvi': os.path.join(my_path, './OUTPUTS/NDVI_WholeField_tiffs/'+farm+'/'),
+    'treatment_ndvi': os.path.join(my_path, './OUTPUTS/NDVI_AOI_tiffs/'+farm+'/'),
+    'treatment_rgb': os.path.join(my_path, './OUTPUTS/RGB_AOI_tiffs/'+farm+'/'),
+    'ndvi_csv': os.path.join(my_path, './OUTPUTS/NDVI_csv/'+farm+'/'),
+    'mean_csv': os.path.join(my_path, './OUTPUTS/mean_NDVI/')
+}
+
+# Create output parent folder
+if not os.path.exists(os.path.join(my_path,'./OUTPUTS/')):
+    os.makedirs(os.path.join(my_path,'./OUTPUTS/'))
+
+# Create output paths above if they dont exist
+for key in output_dict.keys():
+    path = output_dict[key]
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+# do the things!
+allTheThings()
